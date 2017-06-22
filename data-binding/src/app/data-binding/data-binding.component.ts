f25c4184399778ab0ea0803b1e5583a995c66efe
Guilherme de Paiva <guilherme.santos@eigmercados.com.br>
2017-06-22 13:49:11 -0300

    Utilizando data-binding

---
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding',
+  templateUrl: './data-binding.component.html',
+  styleUrls: ['./data-binding.component.css']
+})
+export class DataBindingComponent implements OnInit {
+
+  url: string = 'http://loiane.com';
+  cursoAngular: boolean = true;
+  urlImagem = 'http://www.gazetadopovo.com.br/viver-bem/wp-content/uploads/2016/01/pinguim.jpg';
+
+  getValor(){
+    return 1;
+  }
+
+  getCurtirCurso(){
+    return true;
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
