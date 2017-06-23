793574115020d56f8b6c494253e76c394a870c18
Guilherme de Paiva <guilherme.santos@eigmercados.com.br>
2017-06-23 09:15:57 -0300

    Utilizando eventos no angular

--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -11,12 +11,34 @@ export class DataBindingComponent implements OnInit {
   cursoAngular: boolean = true;
   urlImagem = 'http://www.gazetadopovo.com.br/viver-bem/wp-content/uploads/2016/01/pinguim.jpg';
 
+  valorAtual: string = '';
+  valorSalvo: string = '';
+
+  isMouseOver: boolean = false;
+
   getValor(){
     return 1;
   }
 
   getCurtirCurso(){
     return true;
+  }
+
+  botaoClicado(){
+    alert('Botão clicado');
+  }
+
+  onKeyUp(event: KeyboardEvent){
+    console.log((<HTMLInputElement> event.target).value);
+    this.valorAtual = (<HTMLInputElement> event.target).value;
+  }
+
+  salvarValor(valor){
+    this.valorSalvo = valor;
+  }
+
+  onMouseOverOut(){
+    this.isMouseOver = !this.isMouseOver;
   }
 
   constructor() { }
